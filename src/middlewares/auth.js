d7b55fe2bf1c47c8a9690bc06f8eb6030a9ea333
adit-ya15 <aditya262701@gmail.com>
2026-02-09 21:41:39 +0530

    Write the auth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,11 +1,31 @@
-const adminAuth = (req,res,next) => {
-    const token = "stk";
-    const isAuthorized = token === "stk";
-    if(!isAuthorized){
-        res.status(401).send("unauthorizd admin");
-    }else{
+const jwt = require("jsonwebtoken")
+const User = require("../models/user")
+
+const userAuth = (req,res,next) => {
+    try {
+        const {token} = req.cookies;
+
+        if(!token){
+            throw new Error("Token is not valid");
+        }
+
+        const decodedObj = jwt.verify(token,"DevTinder@4648h");
+
+        const {_id} = decodedObj;
+
+        const user = User.findById(_id);
+
+        if(!user){
+            throw new Error("User not find")
+        }
+
+        req.user = user;
         next();
+
+    } catch (error) {
+        res.status(400).send("Error : ",error.message)
     }
+    
 }
 
-module.exports = {adminAuth}
+module.exports = {userAuth}
