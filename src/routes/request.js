54920997fe55f5958a2ce4953cb3619674841e43
adit-ya15 <aditya262701@gmail.com>
2026-02-13 22:48:02 +0530

    Implemented some importand validation in Connection request route

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,11 +1,51 @@
 const express = require("express");
 const {userAuth} = require("../middlewares/auth")
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const requestRouter = express.Router();
 
-requestRouter.post("/sendConnectionRequest",userAuth,async(req,res) =>{
-    const user = req.user;
-    res.send(user.firstName+" Connection request send")
+requestRouter.post("/request/send/:status/:toUserId",userAuth,async(req,res) =>{
+    try {
+        const fromUserId = req.user._id;
+        const toUserId = req.params.toUserId;
+        const status = req.params.status;
+
+        const allowedStatus = ["interested","ignored"];
+
+        if(!allowedStatus.includes(status.toLowerCase())){
+            return res.status(400).json({message : `${status} is not allowed here`})
+        }
+
+        const isValidToUser = User.findOne({toUserId});
+
+        if(!isValidToUser){
+            return res.status(400).json({message : "User not found"})
+        }
+
+        const existedRequest = ConnectionRequest.findOne({
+            $or: [
+                {fromUserId,toUserId},
+                {fromUserId : toUserId, toUserId : fromUserId}
+            ]
+        })
+
+        if(existedRequest) {
+            return res.status(400).json({message : `Request already exist`})
+        }
+
+        const request = new ConnectionRequest({
+            fromUserId,
+            toUserId,
+            status
+        })
+
+        await request.save();
+        res.status(200).send("Connection request send successfully")
+
+    } catch (error) {
+        res.status(400).send("Err : " + error.message)
+    }
 })
 
 
