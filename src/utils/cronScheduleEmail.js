22b458bdcd2a70e3d703a0a3a1db02a2a6bd2c3e
adit-ya15 <aditya262701@gmail.com>
2026-03-05 17:16:29 +0530

    implement crom job sceheduling to send emails to user regarding their connection requests

--- src/utils/cronScheduleEmail.js
+++ src/utils/cronScheduleEmail.js
@@ -1,5 +1,36 @@
 const cron = require("node-cron");
+const { subDays, startOfDay, endOfDay } = require("date-fns")
+const ConnectionRequest = require("../models/connectionRequest")
+const sendEmail = require("./sendEmail")
 
-cron.schedule("* * * * * * ", () => {
-    
+const yesterDay = subDays(new Date(), 0);
+const start = startOfDay(yesterDay);
+const end = endOfDay(yesterDay);
+
+cron.schedule("* * * * * ", async () => {
+    try {
+        const pendingRequests =await ConnectionRequest.find({
+            status: "interested",
+            createdAt: {
+                $gte: start,
+                $lt: end
+            }
+        }).populate("fromUserId toUserId")
+
+        const emails = [...new Set(pendingRequests.map((req) => req.toUserId.email))]
+        console.log(emails);
+
+        for (const email of emails) {
+            try {
+                const res = await sendEmail(email, "Pending Requests", "You have connection requests pending so please review them", "<h1>Review Requests</h1>");
+                console.log(res);
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    } catch (error) {
+        console.error(error);
+    }
 })
+
+
