4efa27a428cb68aa86354cc9234ab4f479bff9b4
adit-ya15 <aditya262701@gmail.com>
2026-03-12 22:15:46 +0530

    Implemented payment verification route through webhook

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -4,6 +4,8 @@ const instance = require("../config.js/razorpay");
 const paymentRouter = express.Router();
 const membershipType = require("../costants");
 const Payment = require("../models/Payment");
+const {validateWebhookSignature} = require("razorpay/dist/utils/razorpay-utils");
+const User = require("../models/user");
 
 paymentRouter.post("/payment/create",userAuth,async(req,res) => {
     try {
@@ -37,6 +39,46 @@ paymentRouter.post("/payment/create",userAuth,async(req,res) => {
     } catch (error) {
         return res.status(500).json({message : error.message})
     }
+})
+
+paymentRouter.post("/payment/webhook",async(req,res) => {
+    try {
+        const webhookSignature = req.get("X-Razorpay-signature");
+
+        const isWebhookValid = validateWebhookSignature(
+            JSON.stringify(req.body),
+            webhookSignature,
+            process.env.RAZORPAY_WEBHOOK_SECRET
+        );
+
+        if(!isWebhookValid){
+            return res.status(400).json({message : "Webhook signature is invalid"});
+        }
+
+        const paymentDetails = req.body.payload.payment.entity;
+
+        const payment = await Payment.findOne(({orderId : paymentDetails.orderId}));
+        payment.status = paymentDetails.status;
+
+        await payment.save();
+
+        const user = await User.findOne({_id : payment.userId});
+        user.isPremium = true;
+        await User.save();
+
+        return res.status(200).json(({message:"Webhook received successfully"}));
+
+    } catch (error) {
+        return res.status(500).json({message : error.message})
+    }
+})
+
+paymentRouter.get("/premium/verify",async(req,res) => {
+    const user = req.user.toJSON();
+    if(user.isPremium){
+        return res.json({isPremium : true});
+    }
+    return res.json({isPremium : false});
 })
 
 module.exports = paymentRouter;
