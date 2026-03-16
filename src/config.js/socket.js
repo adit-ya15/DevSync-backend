1b783372fe57fbcb87ba4a6b3753a8fc605e468e
adit-ya15 <aditya262701@gmail.com>
2026-03-16 23:42:45 +0530

    Implement chat functionality with message handling in socket and create chat route

--- src/config.js/socket.js
+++ src/config.js/socket.js
@@ -1,22 +1,45 @@
 const socket = require("socket.io");
+const Chat = require("../models/chat");
 
 const initializeSocket = (server) => {
-    const io = socket(server,{
-        cors : {
-            origin : "http://localhost:5173"
+    const io = socket(server, {
+        cors: {
+            origin: "http://localhost:5173"
         }
     })
 
-    io.on("connection",(socket) => {
-        socket.on("joinChat",({userId,targetUserId}) => {
-            const roomId = [userId,targetUserId].sort().join("A");
+    io.on("connection", (socket) => {
+        socket.on("joinChat", ({ userId, targetUserId }) => {
+            const roomId = [userId, targetUserId].sort().join("A");
             socket.join(roomId);
         })
-        socket.on("sendMessage",({firstName,userId,targetUserId,text}) => {
-            const roomId = [userId,targetUserId].sort().join("A");
-            io.to(roomId).emit("messageReceived",{firstName,text})
+        socket.on("sendMessage", async ({ firstName, userId, targetUserId, text }) => {
+            try {
+                const roomId = [userId, targetUserId].sort().join("A");
+
+                let chat = await Chat.findOne({
+                    participants : {$all : [userId,targetUserId]}
+                })
+
+                if(!chat){
+                    chat = new Chat({
+                        participants : [userId,targetUserId],
+                        messages : []
+                    })
+                }
+
+                chat.messages.push({
+                    senderId : userId,
+                    text
+                })
+
+                await chat.save();
+                io.to(roomId).emit("messageReceived", { firstName, text });
+            } catch (error) {
+                console.log(error.message)
+            }
         })
-        socket.on("disconnect",() => {})
+        socket.on("disconnect", () => { })
     })
 }
 
---
+++ src/routes/chat.js
@@ -0,0 +1,28 @@
+const Chat = require("../models/chat");
+
+const express = require(express)
+
+const chatRouter = express.router();
+
+chatRouter.get("/chats/:targetUserId",userAuth,async(req,res) => {
+    const targetUserId = req.params;
+    const userId = req.user._id;
+
+    try {
+        let chat = await Chat.findOne({
+            participants : {$all : [userId,targetUserId]}
+        });
+
+        if(!chat){
+            chat = new Chat({
+                participants : [userId,targetUserId],
+                messages : []
+            })
+        }
+
+        await chat.save();
+        res.json({chat});
+    } catch (error) {
+        res.status(404).json({message : error.message})
+    }
+})
